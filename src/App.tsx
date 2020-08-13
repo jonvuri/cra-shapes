989bd77904f9f456acf727cce76fc8965427660b
Jonathan Rajavuori <jrajav@gmail.com>
2020-08-13 15:06:02 -0500

    Add react-shapes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Circle } from 'react-shapes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Circle r={150} fill={{ color: 'rgba(0,0,0,0.5)' }} stroke={{ color: '#AA00FF' }} strokeWidth={5} />
       </header>
     </div>
   );
