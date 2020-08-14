f9bd14caa19f4e5631b845be066c986c7105c4e5
Jonathan Rajavuori <jrajav@gmail.com>
2020-08-14 15:13:43 -0500

    Add slider to control circle radius

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Circle } from 'react-shapes';
+
+import { Slider } from '@rmwc/slider';
+import '@material/slider/dist/mdc.slider.css';
+
 import './App.css';
 
 function App() {
+  const [radius, setRadius] = useState(150)
+
   return (
     <div className="App">
       <header className="App-header">
-        <Circle r={150} fill={{ color: 'rgba(0,0,0,0.5)' }} stroke={{ color: '#AA00FF' }} strokeWidth={5} />
+        <div className="App-controls">
+          {radius}
+          <Slider className="circle-slider" min={20} max={500} value={radius} discrete step={1}
+            onInput={(event) => {
+              const newRadius = event.detail.value
+
+              setRadius(newRadius)
+            }}
+          />
+        </div>
+        <Circle r={radius} fill={{ color: 'rgba(0,0,0,0.5)' }} stroke={{ color: '#AA00FF' }} strokeWidth={5} />
       </header>
     </div>
   );
