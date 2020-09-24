71ffb51a9843a9b47b637f265cc84b3c7d093ae0
Jonathan Rajavuori <jrajav@gmail.com>
2020-09-24 17:08:18 -0500

    Add error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ErrorBoundary, FallbackProps} from 'react-error-boundary';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from "./store";
 
+function ErrorFallback({error, resetErrorBoundary}: FallbackProps) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error?.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
+
 const render = () => ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
